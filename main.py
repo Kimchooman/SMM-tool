b1f47e0468d9c4b7b95a729fb3bc01eb88f74cae
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2020-05-29 10:17:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,147 +1,152 @@
 import json
 import instaloader
-import csv
 from datetime import datetime
-import os
+import subprocess
+import random
+
+def copy2clip(txt):
+    cmd='echo '+txt.strip()+'|clip'
+    return subprocess.check_call(cmd, shell=True)
+
+def delta_days(date: list):
+	months = date[0]
+	days = date[1]
+
+	c_day = datetime.today().days
+	c_month = datetime.today().month
+	
+	d_day = c_day - days
+	d_month = c_month - months
+
+	return d_days + (d_months * 30)
 
 cmd = input("What would you like to do?  \n \'unfollow\' or \'follow\' or \'update\' \n --> ")
 
-old_prospects_path = "old_prospects.csv"
-current_prospect_path = "current_prospects.csv"
-future_prospect_path = "future_prospects.csv"
+prospect_path = "prospects.json"
 
 unfollow_period = 3 #days
 action_batch_size = 100
 
-prospect_source_accounts = ["rileyjacksonx.o"] #"medicalertca", "dothealthhq"]
-
-
-def remove_known_prospects(file_name, name_list):
-
-	del_marks = []
-
-	with open(file_name, "r") as jsondata:
-		jsonreader = json.load(jsondata)
-	
-		for prospect in name_list:
-
-			for person in list(jsonreader["current"].keys()):
-				if prospect == person["user"]
-
-	for person in del_marks:
-		name_list.remove(person)
-
-def save_over_file(file_name, new_data):
-
-	os.remove(file_name)
-
-	with open(file_name) as csv_data:
-
-		file_writer = csv.writer(file_name)
-
-		for line in new_data:
-			file_writer.writerow(line)
+prospect_source_accounts = ["immanueljones8"]#"medicalertca", "dothealthhq"]
 
 if  cmd == "unfollow":
-	return_ar = []
+	
+	with open(prospect_path, "r") as json_file:
+		data = json.load(json_file)
 
-	with open(current_prospect_path) as csv_file:
-		csv_reader = csv.reader(csv_file)
+		current_people = data["current"].keys()
 
 		count = 0
-		for person in csv_reader:
+		for target in current_people:
 
-				if count < action_batch_size:
-					if datetime.datetime.today().day - person[1] >= unfollow_period:
-					
-						count += 1
-						return_ar.append(person)
+			copy2clip(target)
 
-	with open(old_prospects_path) as csv_file:
+			input(f"unfollow -->  {target}  <--")
+
+			temp_holder = data["current"][target]
+			data["current"].pop(target)
+			data["past"][target] = temp_holder
+
+			with open(prospect_path, "w") as json_file:
+
+				json.dump(data, json_file)
+			if count >= action_batch_size:
+				break
+			
+			count += 1
 		
-		csv_writer = csv.writer(csv_file)
 
-		for person in return_ar:
-			input(f"Unfollow -->  {person[0]}  <--")
-			csv_writer.writeline(person)
-		
-	for person in return_ar:
-		csv_reader.remove(person)
-
-		save_over_file(current_prospect_path, csv_reader)
 
 elif cmd == "follow":
 
-	working_ar = []
+	with open(prospect_path, "r") as json_file:
+		data = json.load(json_file)
 
-	with open(future_prospect_path) as csv_file:
+	count = 0
 
-		csv_reader = csv.reader(csv_file)
+	future_prospects = list(data["future"].keys())
+	
+	del_marks = []
 
-		count = 0
+	for person in future_prospects:
 
-		for person in csv_reader:
-			if count < action_batch_size:
-				
-				working_ar.append(person)
-				count += 1 
+		copy2clip(person)
+		input(f"\nFollow -->  {person}  <--\n")
 
-			else:
-				break
+		temp_holder = data["future"][person]
+
+		data["future"].pop(person, None)
+
+		data["current"][person] = temp_holder
+
+		with open(prospect_path, "w") as json_file:
+			json.dump(data, json_file)
 		
-	for person in working_ar:
-		csv_reader.remove(person)
+		count += 1
 
-	save_over_file(future_prospect_path, csv_reader)
+		if count >= action_batch_size:
+			break
+	
+elif cmd == "RESET":
 
-	with open(current_prospect_path) as csv_file:
+	num = random.randint(1,100)
+		
+	if input(f"Type: {num}  -->") == num:
+		with open(prospect_path, "w") as json_file:
 
-		for person in working_ar:
+			save = {"current": {},"past": {},"future": {}}
 
-			input(f"Follow -->  {person[0]}  <--")
-			csv_writer.writeline(person)
+			json.dump(save, json_file)
 
 elif cmd == "update":
 
 	new_prospects = []
 	
 	L = instaloader.Instaloader()
-
 	L.login("l1b0j2", "mouseking") 
 
 	for account in prospect_source_accounts:
 		
 		profile = instaloader.Profile.from_username(L.context, account)
-
+		
 		for follower in profile.get_followers():
-
 			for prospect in new_prospects:
+
 				if prospect == follower:
 					break
 				
 			else:
-				new_prospects.append(follower.username)
+				new_prospects.append(follower)
 	
-	remove_known_prospects(old_prospects_path, new_prospects)
-	remove_known_prospects(current_prospect_path, new_prospects)
-	remove_known_prospects(future_prospect_path, new_prospects)
-
-	print(new_prospects)
-	final_ar = []
-
-	for prospect in new_prospects:
-		
-		_ = []
-
-		_.append(prospect)
-		_.append(datetime.today().day)
-
-		final_ar.append(_)
 	
-	with open(future_prospect_path) as csv_file:
+	with open(prospect_path, "r") as json_file:
 
-		csv_writer = csv.writer(csv_file)
+		data = json.load(json_file)
+		#data = json.loads(data)
 
-		for line in final_ar:
-			print(line)
-			csv_writer.writerow(line)
+		for prospect in new_prospects:
+
+			try:
+				try:
+					try:
+						data["current"][prospect.username]
+						new_prospects.remove(prospect)
+
+					except:
+						data["future"][prospect.username]
+						new_prospects.remove(prospect)
+				
+				except:
+					data["past"][prospect.username]
+					new_prospects.remove(prospect)
+
+			except:
+
+				data["future"][prospect.username] = {}
+				data["future"][prospect.username]["know_since"] = [datetime.today().month, datetime.today().day, datetime.today().year]
+				data["future"][prospect.username]["is_business"] = prospect.is_business_account
+
+
+	with open(prospect_path, "w") as jsonfile:
+
+		json.dump(data, jsonfile)
